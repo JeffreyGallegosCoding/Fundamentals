be505a9924527b12ad6a9774d2c9e6abdc2a3ef8
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-03-26 15:31:27 -0700

    Used different examples in order to practice the coding fundamentals.

---
+++ conditionals-and-functions.js
@@ -0,0 +1,75 @@
+/*
+*Conditionals, Functions, Scope, and Loops
+ */
+
+/*
+//Equals
+let equals = 1 === 1;
+console.log(equals);
+
+///Greater than
+let greaterthan = 5 > 1;
+
+// Less than
+let lessthan = 2 < 10;
+
+// Greater than equals
+let greaterthaneq = 5 >= 5;
+
+//Less than equals
+let lessthaneq = 4 <= 9;
+
+//Not Equals
+let notequals = 5 !== 2;
+*/
+
+/*
+let storeA = 4.40;
+let storeB = 3.40;
+
+function comparestoreprices(storeA, storeB) {
+	let storeAislower = storeA < storeB;
+	if(storeAislower) {
+		console.log("Store A has a lower price.")
+	} else if(storeB < storeA) {
+		console.log("Store B has a lower price.")
+	} else {
+		console.log("Their prices are equal.")
+	}
+}
+
+//comparestoreprices(10, 5);
+//comparestoreprices(7, 10);
+
+function squareNum(number) {
+	return number * number;
+}
+
+let squaredNumber = squareNum(7);
+console.log(squaredNumber);
+*/
+/*
+let x = 10;
+
+function addnumbers(n, m, x) {
+	console.log(x);
+	if(1===1) {consolelog.B}
+		let B = 8;
+	return n + m;
+}
+*/
+/*
+let ourArray = [1, 2, 3, 4, 5, 1, 7];
+
+let arrLen = ourArray.length;
+for(let i = 0; i<arrLen.length; i++) {
+	//console.log("i is equal to: " + i);
+	console.log(ourArray [i]);
+}
+*/
+
+let x = 0;
+while(x<10) {
+	console.log('Ran');
+	x = x + 1;
+}
---
+++ objects-apis.js
@@ -0,0 +1,26 @@
+/*
+* Objects, Interfaces, and API's
+ */
+/*
+let dog = {
+	name: "Steve",
+	color: "brown",
+	breed: "pug",
+	size: "Small",
+	bark: function(typeofbark){
+		console.log("Bark!");
+	},
+};
+*/
+
+function x(y) {
+	y.num = y.num + 5;
+	console.log(y);
+}
+
+let y = {
+	name: "Tom",
+	num: 10,
+};
+x(y);
+console.log(y);
